3d07dbf9295589e56efd0dc90db1e20803cb5277
saad <21ktele0630@uetpeshawar.edu.pk>
2026-06-23 17:50:42 +0500

    add base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/multiply-agent-frontend/',
   preview: {
     allowedHosts: ['lobster-app-9qdmi.ondigitalocean.app'],
   },
