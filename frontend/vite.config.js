23c67dde4dfbe60a585cbe3e077235d39516812e
saad <21ktele0630@uetpeshawar.edu.pk>
2026-06-23 18:03:50 +0500

    remove base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/multiply-agent-frontend/',
   preview: {
     allowedHosts: ['lobster-app-9qdmi.ondigitalocean.app'],
   },
